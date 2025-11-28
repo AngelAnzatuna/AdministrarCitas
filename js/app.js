0857d78dc6b84cde706f2fb2577a7df48821d55a
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-11-28 19:10:05 +0100

    Agregar notificación de error al enviar el formulario si hay campos obligatorios vacíos

--- js/app.js
+++ js/app.js
@@ -32,7 +32,34 @@ function datosCita(e) {
 function submitCita(e) {
     e.preventDefault();
     if(Object.values(citaObj).some(valor => valor.trim() === '')) {
-        console.log('Todos los campos son obligatorios');
+        new Notificacion({
+            texto: 'Todos los campos son obligatorios',
+            tipo: 'error'
+        })
         return;
+    }
+}
+
+class Notificacion {
+    constructor({texto, tipo}) {
+        this.texto = texto
+        this.tipo = tipo
+
+        this.mostrar()
+    }
+
+    mostrar() {
+        // Crear la notificación
+        const alerta = document.createElement('DIV');
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
+
+        // Si es de tipo error, agrega una clase
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+
+        // Mensaje de error
+        alerta.textContent = this.texto;
+
+        // Insertar en el DOM
+        formulario.parentElement.insertBefore(alerta, formulario);
     }
 }
