67bd66d4c046837967e9964bd028dee31ecaaf67
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-11-28 20:55:43 +0100

    Agregar eliminación automática de notificaciones después de 3 segundos

--- js/app.js
+++ js/app.js
@@ -53,6 +53,10 @@ class Notificacion {
         const alerta = document.createElement('DIV');
         alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
 
+        // Eliminar alertas duplicadas
+        const alertaPrevia = document.querySelector('.alert');
+        alertaPrevia?.remove();
+
         // Si es de tipo error, agrega una clase
         this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
 
@@ -61,5 +65,10 @@ class Notificacion {
 
         // Insertar en el DOM
         formulario.parentElement.insertBefore(alerta, formulario);
+
+        // Quitar después de 3 segundos
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
     }
 }
