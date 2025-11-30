1723ab8e6678169622a7b5dc68e5e21753869d33
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-11-30 21:43:53 +0100

    Agregar generación de ID único al reiniciar objeto de cita

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ formulario.addEventListener('submit', submitCita);
 
 // Objeto de Cita
 const citaObj = {
+    id: generarId(),
     paciente: '',
     propietario: '',
     email: '',
@@ -164,6 +165,7 @@ function submitCita(e) {
 
 function reiniciarObjetoCita() {
     // Reiniciar el objeto
+    // citaObj.id = generarId();
     // citaObj.paciente = '';
     // citaObj.propietario = '';
     // citaObj.email = '';
@@ -171,12 +173,17 @@ function reiniciarObjetoCita() {
     // citaObj.sintomas = '';
 
     Object.assign(citaObj, {
+        id: generarId(),
         paciente: '',
         propietario: '',
         email: '',
         fecha: '',
         sintomas: ''
     })
+}
+
+function generarId() {
+    return Math.random().toString(36).substring(2) + Date.now();
 }
 
 function cargarEdicion(cita) {
